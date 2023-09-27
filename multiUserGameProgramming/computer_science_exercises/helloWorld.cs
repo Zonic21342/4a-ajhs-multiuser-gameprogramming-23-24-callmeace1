15ad260b2d02f93dfa06104e4839b21291b2865c
callmeace1 <s11252673@students.duvalschools.org>
2023-09-27 12:44:59 -0400

    cg

---
+++ multiUserGameProgramming/computer_science_exercises/helloWorld.cs
@@ -0,0 +1,98 @@
+
+
+
+
+
+        class controlFlow 
+        {
+            0 references 
+            static void Main (string [] args)
+            }
+                // DECLARATIONS
+                string favColor = "red";
+                Int LuckyNumber = 7;
+                float my GPA = 3.3F;
+                int myAge = 43;
+                bool PineappleOnPizza = No;
+
+                // Statements -- Checks for a single condition! 
+                if (favColor == "red") {
+                    console. WriteLine("red with fire");
+
+                }
+
+                if (LuckyNumber == 7) {
+                    Console.WriteLine ("7 is odd");
+                }
+
+                // if - else Statement -- Checks for a string condition , but jas two 
+                if (myAge > 15) {
+                    console.WriteLine("You are eligible to drive");
+                } else {
+                    Console.WriteLine("START WALKING SCRUB!");
+                }
+
+                // if -- else if -- else -- Checks multiple outcomes.
+                if (myGPA = 4.0F) {
+                Console.WriteLine("ongrats on straight A grades!");   
+                } else if (myGPA >= 3.0F) {
+                    Console.WriteLine("Congrats on making honor rolls!");
+                }else if (my GPA >= 2.0F) {
+                    Console.WriteLine("You are graduation ready!");
+                } else if (myGPA >= 1.0F){
+                Console.WriteLine("You should probaly study!");   
+                }
+                // WHEN CHECKING NUMBERS VALUES, START WITH THE HIGHEST NUMBER!!!!
+
+                if (PineappleOnPizza == true) {
+                Console.WriteLine("Eww, thats gross. You most be a boomer. How pld are you");   
+                }   if (myAGE > 60){
+                Console.WriteLine("Just as a suspected! What was it like having a dinisour growing up");   
+                } else {
+                Console.WriteLine("Ok your not a boomer but still have no taste in food.");   
+                }
+                
+                } else {
+                Console.WriteLine("Glad to see you have common sense!=");   
+                    
+                }
+
+                // flor loop -- best for when you know # of iterations needed.
+                
+                |   for (statement1; statement2; statement 3)   {
+                    code or loop.
+                }
+                statement1 is exceuted ONCE BEFORE the loop starts.
+                statement2 is a CONDITONAL that is checked EVERYTIME BEFORE loop 
+                statement3 is executed EVERYTIME after the loop executes. 
+                
+            
+
+               
+                for (int i = 0; i < 101; i++) {
+                    Console.WriteLine ("Outlier: " + i);
+                }
+
+                // Nested loops
+                for (int i = 1; i <=3; j++) {
+                    Console.WriteLine("Inner: " +  j);
+
+                }
+
+                // while loop -- best used when # of interactions needed is unknown 
+                int x = 0; 
+                while (x < 1000){
+                    Console.WriteLine(""+ x);
+                    
+                
+                }
+
+                // Special Keywords 
+                // break will immediately exit a LOOP or an IF/ELSE IF/ ELSE block. 
+                for (int i = -0; i < 101; i++)
+                Console.WriteLine ("" + i);
+                if (i== 50) {
+                 continue;   
+                }
+            
+            
